46b0611a55a41dad9f597baa3f69389365d1cdf4
Michael <2018118090@ub.edu.bz>
2024-04-17 10:25:03 -0600

    some weird thing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
 import express, {Request, Response} from "express";
 import bodyParser from 'body-parser';
+import fs from 'fs';
 
-
-
+//🤓 figuring out to serve files to the front end
 
 //Loading environment variables
 dotenv.config();
@@ -18,7 +18,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //Defining routes
 app.get('/home', (req: Request, res: Response) => {
-
+    try{
+        const fileStream = fs.createReadStream('./file-server/funny.mp4');
+        fileStream.pipe(res);
+    
+        return res.status(200);
+    } catch (error: any){
+        console.log(error);
+    }
 });
 
 //Starting the server
