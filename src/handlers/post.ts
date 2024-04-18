1ba5b1deafa52282e5731abbf2fdcf6a96234f73
Michael <2018118090@ub.edu.bz>
2024-04-18 15:54:00 -0600

    added a post to the static posts

--- src/handlers/post.ts
+++ src/handlers/post.ts
@@ -9,7 +9,7 @@ enum mediaTypeEnum {
     mp4 = ".mp4",
 }
 
-const post = {
+const post_1 = {
     "id": 1,
     "type": mediaTypeEnum.png,
     "title" : "They sap Mana",
@@ -20,10 +20,32 @@ const post = {
     "nsfw": false
 }
 
+const post_2 = {
+    "id": 2,
+    "type": mediaTypeEnum.png,
+    "title" : "Placeholder",
+    "alt": "a place holder image",
+    "path" : "images/placeholder.png",
+    "description" : "A place holder image",
+    "date" : "18/04/2024",
+    "nsfw": false
+}
+
 
 //handler
 export const getPost = async (req: Request, res: Response) => {
     try{
+
+        let post = null
+
+        if(req.params.id == "1") {
+            post = post_1;
+        } else if (req.params.id == "2") {
+            post = post_2;
+        } else {
+            return res.status(404).json({message: "no post"});
+        }
+
         const logTime = new Date().toString();
         console.log("📤 Successfully Processed Post Request : " + logTime);
         return res.status(200).json(post);
