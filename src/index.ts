ab0e850623f781a317f730fd34476505b9060a79
Michael <2018118090@ub.edu.bz>
2024-04-18 10:15:58 -0600

    made backend send json to the frontend , going to make the frontend do pretty things now

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import 'dotenv/config';
 import express, {Request, Response} from "express";
 import bodyParser from 'body-parser';
-import fs from 'fs';
 import path from 'path';
 import { getGif, getImage, getVideo } from './handlers/media';
 
+//Free Space
+const post = {
+    "id": 1,
+    "path" : "mana_sapper.png",
+    "title" : "They sap Mana",
+    "date" : "18/04/2024",
+    "description" : "A painting made when I learning painting",
+}
 
 //Express app
 const app = express();
+
 //env
 const PORT = process.env.PORT || 3000;
 const DIR = process.env.DIR as string;
@@ -15,19 +23,25 @@ const DIR = process.env.DIR as string;
 //adding necessary middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(DIR as string, 'public')));
+app.use(express.static(path.join(DIR, 'public')));
 
 //Defining routes
-
-
-//🤓 figuring out to serve files to the front end
-
 
 //Media handlers TODO: remember to put sanitization middleware on these routes
 app.get('/image/:filename', getImage);
 app.get('/gif/:filename', getGif);
 app.get("/video/:filename", getVideo);
+
+app.get("/post/:id", (req: Request, res: Response) => {
+    try{
+        const logTime = new Date().toString();
+        console.log("📤 Successfully Processed Post Request : " + logTime);
+        return res.status(200).json(post);
+    } catch (error: any){
+        console.log(error);
+        res.status(500).json("some ting wong!");
+    }
+});
 
 app.get('/hello', (req: Request, res: Response) => {
     return res.status(200).send("Hello World!");
